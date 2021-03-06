59aa51245d098b99cd032a907b748c409a166712
ste163 <samtedwards@pm.me>
2021-03-06 14:39:15 -0500

    restructured for main and renderer directories; added vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  pluginOptions: {
+    electronBuilder: {
+      // Use this to change the entrypoint of your app's main process
+      mainProcessFile: "src/main/background.js",
+      // Use this to change the entry point of your app's render process. default src/[main|index].[js|ts]
+      rendererProcessFile: "src/renderer/main.js"
+    }
+  }
+};
