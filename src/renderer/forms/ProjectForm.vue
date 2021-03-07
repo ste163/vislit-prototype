f0e3f90743db86a91628ad2a21c615f3b1ccd0f4
ste163 <samtedwards@pm.me>
2021-03-07 12:23:29 -0500

    added notes for project form structure

--- src/renderer/forms/ProjectForm.vue
+++ src/renderer/forms/ProjectForm.vue
@@ -1,6 +1,17 @@
 // If passing in something we already have, change to edit form
 <template>
   <form @submit.prevent="handleSubmit">
+    <!-- Separate these into 2 forms with a step 1 & step 2 -->
+
+    <!-- PROJECT FORM -->
+    <!-- Project Name -->
+    <!-- Type -->
+    <!-- Short description -->
+    <!-- Start Date -->
+
+    <!-- GOAL FORM -->
+    <!-- Word count goal per day -->
+    <!-- Goal Frequency (daily, weekly, monthly) -->
     <button type="submit">Save</button>
   </form>
 </template>
