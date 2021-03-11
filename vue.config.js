918e70798395117ccf6adb5aaede63c37f77e2ec
ste163 <samtedwards@pm.me>
2021-03-11 13:04:04 -0500

    fixed undefined by adding nodeIntegration

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,7 @@ module.exports = {
   },
   pluginOptions: {
     electronBuilder: {
+      nodeIntegration: true,
       // Use this to change the entrypoint of your app's main process
       mainProcessFile: "src/main/background.js",
       // Use this to change the entry point of your app's render process. default src/[main|index].[js|ts]
