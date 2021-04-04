0e045b2187deb1975d795241e30288478191a9af
ste163 <samtedwards@pm.me>
2021-04-04 12:15:16 -0400

    added hardcoded table with no styling

--- src/renderer/views/ViewProgress.vue
+++ src/renderer/views/ViewProgress.vue
@@ -5,8 +5,41 @@
     </template>
 
     <template #content>
-      <article>Progress Bar</article>
-      <article>Sorted Excel-like Table of Progress</article>
+      <section class="card">
+        <th>
+          <tr>
+            <th>Date</th>
+            <th>Words Written</th>
+            <th>Proofread</th>
+            <th>Edited</th>
+            <th>Revised</th>
+          </tr>
+          <!-- table with computed property rows, hard-coded for now -->
+          <tbody name="table">
+            <tr>
+              <td>12/21/2021</td>
+              <td>350</td>
+              <td>X</td>
+              <td>_</td>
+              <td>_</td>
+            </tr>
+            <tr>
+              <td>12/21/2021</td>
+              <td>350</td>
+              <td>X</td>
+              <td>_</td>
+              <td>_</td>
+            </tr>
+            <tr>
+              <td>12/21/2021</td>
+              <td>350</td>
+              <td>X</td>
+              <td>_</td>
+              <td>_</td>
+            </tr>
+          </tbody>
+        </th>
+      </section>
     </template>
   </view-template>
 </template>
